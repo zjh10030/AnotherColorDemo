84a76ac2bb1ef5346005dd4d0289f5ff625cf000
zjh10030 <zjh10030@vtc.vsc.edu>
2019-02-25 15:37:22 -0500

    Lab updates

--- app/src/main/java/com/example/anothercolordemo/MainActivity.java
+++ app/src/main/java/com/example/anothercolordemo/MainActivity.java
@@ -3,9 +3,15 @@ package com.example.anothercolordemo;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +21,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        addListener();
+    }
+    private void addListener(){
+        EditText.OnEditorActionListener listener = new EditText.OnEditorActionListener(){
+            @Override
+            public boolean onEditorAction(TextView v, int actionID, KeyEvent event) {
+                if (actionID == EditorInfo.IME_ACTION_DONE){
+                    submitColor(null);
+
+                    InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(),0);
+
+                    return true;
+                }
+
+                return false;
+
+            }
+        };
+
+       EditText input = findViewById(R.id.color_name_input);
+        input.setOnEditorActionListener(listener);
+
         /**
          * Get color name from the input field and set the background of the output to that color
          * @param view
@@ -22,9 +51,11 @@ public class MainActivity extends AppCompatActivity {
     }
     public void submitColor (View view) {
         EditText input = findViewById(R.id.color_name_input);
-        String name = input.getText().toString().toLowerCase() ;
+        String name = input.getText().toString().toLowerCase();
         setColorByName(name);
-    }
+
+        }
+
 
     /**
      * Sets the background color of the output field
@@ -43,6 +74,19 @@ public class MainActivity extends AppCompatActivity {
 
         Toast.makeText(this,"Color not found", Toast.LENGTH_SHORT).show();
         }
+
+
+
+
+
+
     }
 
+    public void randomColor(View view) {
+        String[] colors = getResources().getStringArray(R.array.colors);
+
+        int whichColor = (int) (colors.length * Math.random());
+
+        setColorByName(colors[whichColor]);
+    }
 }
